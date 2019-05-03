5956a6ce1117916ab1c92f8e9143f8b2b9cd6f64
web-yk5160 <rubystudy0123@gmail.com>
2019-05-03 10:53:04 +0900

    Setting up the API client

---
+++ app/javascript/packs/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export function listTasks () {
+  return axios.get('/tasks.json')
+          .then(function(response){
+            return response.data;
+          })
+}
+
+listTasks().then(function(response){
+  console.log(response);
+});
